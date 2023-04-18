982ee1407833813e320c93f718ad68eb02f9a893
prtribhuvan <pallavitribhuvan1997@gmail.com>
2023-04-18 21:39:21 +0530

    Palindrome String

---
+++ DemoFile/src/com/practice/TestDemo.java
@@ -0,0 +1,45 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class TestDemo {
+
+	  public static void getString(String str) {
+		  
+		String reverse = "";
+		String a = str ;
+		
+		for(int i = str.length()-1 ; i>=0 ;i-- ) {
+			
+			  reverse = reverse + str.charAt(i);
+			
+		}
+		  
+		if(a.equals(reverse)) {
+			
+			System.out.println("palindrome");
+		}else {
+			
+			System.out.println("not palindrome");
+		}
+	  }
+	
+	  public static void main(String[] args) {
+		
+		  System.out.println("Enter String >> ");
+		  Scanner scanner =  new Scanner(System.in);
+		  String s = scanner.next();
+		  
+		  getString(s);
+		  
+		  
+		  
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ DemoFile/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author palla
+ *
+ */
+module DemoFile {
+}
